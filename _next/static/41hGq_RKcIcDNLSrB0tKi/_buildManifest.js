3591b174daebabf471d75db1c1cb7aa3d6e4a39a
dzkb <1517445+dzkb@users.noreply.github.com>
2021-03-16 19:47:38 +0000

    Deploying to gh-pages from @ dzkb/juniorsenior-gh-actions@5cd2e2028a45f12bf5ef053bf0fb1331856342e0 🚀

---
+++ _next/static/41hGq_RKcIcDNLSrB0tKi/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-0c3fd0762f702d573769.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-e82794e4d5cd188e8c88.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
